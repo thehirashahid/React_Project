fa10836dca1ffe706c5ca9eeb6a752baf6127d2f
thehirashahid <hira.shahid@devsinc.com>
2022-12-26 16:33:22 +0500

    Done with registration and login

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Registration from "./registration/Registration";
+import Login from "./registration/Login";
+import Posts from './posts/posts'
+
+const App = () => {
+  return <Posts />;
+};
+
+export default App;
---
+++ src/posts/posts.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Posts = () => {
+    return <>
+        <h2>Hello</h2>
+    </>
+};
+
+export default Posts;
